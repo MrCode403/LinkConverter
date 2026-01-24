69bed4fe88426fc7753a6732b07f86b3ceae04a4
мя.cσdε 403 <singhjagdeep102030@gmail.com>
2026-01-24 17:35:56 +0530

    Update Gradle and Android Gradle Plugin versions in build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+buildscript {
+    val agp_version by extra("9.0.0")
+}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.0.0" apply false
+    id("com.android.application") version "9.0.0" apply false
     id("com.android.library") version "8.0.0" apply false
          
 }
